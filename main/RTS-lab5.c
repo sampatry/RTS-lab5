4ab789461e72b55f7a6a47bd855e2f628adb24c5
sam <sampatry8@gmail.com>
2025-07-14 20:01:25 -0400

    test

--- main/RTS-lab5.c
+++ main/RTS-lab5.c
@@ -1,6 +1,18 @@
-#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include <string.h>
+
+
+
+void setup(){
+
+}
+
 
 void app_main(void)
 {
-
+    setup();
 }
