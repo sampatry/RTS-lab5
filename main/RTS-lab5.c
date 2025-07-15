2d3a85be84915c4eff0e70c32d73962d9a7511e5
sam <sampatry8@gmail.com>
2025-07-15 11:32:50 -0400

    added-task

--- main/RTS-lab5.c
+++ main/RTS-lab5.c
@@ -9,13 +9,35 @@
 #define I2C_SDA_IO           21
 #define I2C_FREQ_HZ          100000
 #define I2C_DEVICE_ADDR      0x68
-#define I2C_REGISTER_ADDR    0x75
+
+#define ESP_SLAVE_ADDR 0x0A
 
 static const char *TAG = "I2C_MASTER";
 
 // Global handles so both setup() and app_main() can use them
 static i2c_master_bus_handle_t i2c_bus_handle;
 static i2c_device_config_t device_config;
+static i2c_master_dev_handle_t dev_handle;
+
+void task1(void *arg) {
+
+    ESP_ERROR_CHECK(i2c_master_bus_add_device(i2c_bus_handle, &device_config, &dev_handle));
+    
+    while (1) {
+        // Write register address to read from
+        uint8_t reg_addr = ESP_SLAVE_ADDR;
+        ESP_ERROR_CHECK(i2c_master_transmit(dev_handle, &reg_addr, 1, pdMS_TO_TICKS(1000)));
+
+        // Read 1 byte from the device
+        uint8_t data;
+        ESP_ERROR_CHECK(i2c_master_receive(dev_handle, &data, 1, pdMS_TO_TICKS(1000)));
+
+        ESP_LOGI(TAG, "Received byte: 0x%02x", data);
+
+        vTaskDelay(pdMS_TO_TICKS(2000));  // wait 2 seconds before reading again
+    }
+}
+
 
 void setup(void)
 {
@@ -40,23 +62,7 @@ void app_main(void)
 {
     setup();
 
-    i2c_master_dev_handle_t dev_handle;
-    ESP_ERROR_CHECK(i2c_master_bus_add_device(i2c_bus_handle, &device_config, &dev_handle));
-
-    while (1)
-    {
-        // Write register address to read from
-        uint8_t reg_addr = I2C_REGISTER_ADDR;
-        ESP_ERROR_CHECK(i2c_master_transmit(dev_handle, &reg_addr, 1, pdMS_TO_TICKS(1000)));
-
-        // Read 1 byte from the device
-        uint8_t data;
-        ESP_ERROR_CHECK(i2c_master_receive(dev_handle, &data, 1, pdMS_TO_TICKS(1000)));
-
-        ESP_LOGI(TAG, "Received byte: 0x%02x", data);
-
-        vTaskDelay(pdMS_TO_TICKS(2000));  // wait 2 seconds before reading again
-    }
+    xTaskCreate(task1, "master-send-task", 1024, NULL, 1, NULL);
 
     // Cleanup if ever reached (usually app_main runs forever)
     ESP_ERROR_CHECK(i2c_master_bus_rm_device(dev_handle));
